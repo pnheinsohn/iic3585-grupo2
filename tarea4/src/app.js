805bf7997676b46024f18ea15314b56943217323
paul <pnheinsohn@uc.cl>
2020-05-12 13:16:47 -0400

    feat: clicking pokemon now callbacks

--- tarea4/src/app.js
+++ tarea4/src/app.js
@@ -1,4 +1,5 @@
 //create an empty array on startup
+let currentPokemon;
 let searchHistory = [];
 const API_BASE = 'https://pokeapi.co/api/v2';
 const API_POKEMON = `${API_BASE}/pokemon`;
@@ -6,22 +7,65 @@ const getPokemonImage = id => `https://raw.githubusercontent.com/PokeAPI/sprites
 const HISTORY_STORAGE_KEY = 'HISTORY_KEY';
 
 /**
- * generate a Pokemon tag
+ * generate a Pokémon tag
  */
 function buildPokemonMarkup(pokemon) {
-    return `<div class="pokemon_item"><img class="pokemon_image" src=${getPokemonImage(pokemon.id)} />
-        <h2 class="pokemon_name">${pokemon.forms[0].name}</h2>
-        <p class="pokemon_description">${pokemon.weight}</p></div>`;
+    currentPokemon = pokemon;
+    const main_pokemon = document.querySelector('#main_pokemon');
+    const pokemon_item = document.createElement('div');
+    const pokemon_stats = document.createElement('table');
+
+    main_pokemon.innerHTML = '';
+
+    pokemon_item.addEventListener('click', addToWishlist);
+    pokemon_item.className = 'pokemon_item';
+    pokemon_item.innerHTML =
+        `<img class="pokemon_image" src=${getPokemonImage(pokemon.id)} />
+        <h6 class="pokemon_name">${pokemon.name}</h6>`;
+
+    pokemon_stats.innerHTML =
+        `<tr>
+            <td><span><strong>Hp:</strong> ${pokemon.stats[5].base_stat}</span></td>
+            <td><span><strong>Atk:</strong> ${pokemon.stats[4].base_stat}</span></td>
+            <td><span><strong>Def:</strong> ${pokemon.stats[3].base_stat}</span></td>
+        </tr>
+        <tr>
+            <td><span><strong>Speed:</strong> ${pokemon.stats[0].base_stat}</span></td>
+            <td><span><strong>Special Atk:</strong> ${pokemon.stats[2].base_stat}</span></td>
+            <td><span><strong>Special Def:</strong> ${pokemon.stats[1].base_stat}</span></td>
+        </tr>`;
+
+    main_pokemon.appendChild(pokemon_item);
+    main_pokemon.appendChild(pokemon_stats);
 }
 
+/**
+ * generate all searched Pokémon imgs
+ */
 function buildPokemonHistory() {
-    let history = '';
-    searchHistory.forEach(pokemon => {
-        history += `<div class="pokemon_item"><img class="pokemon_image" src=${getPokemonImage(pokemon.id)} />
-        <h2 class="pokemon_name">${pokemon.forms[0].name}</h2>
-        <p class="pokemon_description">${pokemon.weight}</p></div>`;
+    const history = document.querySelector('#history');
+    history.innerHTML = '';
+    searchHistory.forEach((pokemon, index) => {
+        const pokemon_item = document.createElement("div");
+        pokemon_item.addEventListener("click", function() {
+            buildPokemonMarkup(searchHistory[index]);
+            updateHistory(searchHistory[index]);
+        });
+        pokemon_item.innerHTML +=
+            `<div class="pokemon_item">
+                <img class="pokemon_image" src=${getPokemonImage(pokemon.id)} />
+                <h6 class="pokemon_name">${pokemon.name}</h6>
+            </div>`;
+        history.appendChild(pokemon_item);
     });
-    return history
+    return;
+}
+
+/**
+ * Adds to wishlist
+ */
+function addToWishlist() {
+    console.log(currentPokemon);
 }
 
 /**
@@ -61,21 +105,13 @@ function updateHistory(pokemon) {
         searchHistory.unshift(pokemon);
     }
     localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(searchHistory));
-    updatePokemonHistoryTag();
-}
-
-/**
- * Update DOM
- */
-function updatePokemonHistoryTag() {
-    document.querySelector('#history').innerHTML = buildPokemonHistory();
+    buildPokemonHistory();
 }
 
 /**
  * loadAnpokemon from the internet and place it on a target element
  */
 async function onSearchClickAsync() {
-    let targetElementId = '#main_pokemon';
     let pokemonId = document.querySelector("#search_input").value;
     try {
         const response = await fetch(`${API_POKEMON}/${pokemonId}`);
@@ -83,9 +119,8 @@ async function onSearchClickAsync() {
             return;
         }
         let pokemon = await response.json();
-        console.log("pokemon", pokemon);
-        document.querySelector(targetElementId).innerHTML = buildPokemonMarkup(pokemon);
-
+        pokemon.name = pokemon.forms[0].name[0].toUpperCase() + pokemon.forms[0].name.slice(1);
+        buildPokemonMarkup(pokemon);
         updateHistory(pokemon);
     } catch (err) {
         console.error(`error ${err}`);
@@ -101,11 +136,11 @@ function getLocalHistory() {
 
 async function onLoadAsync() {
     // Load the history from cache
-    let history = getLocalHistory()
+    let history = getLocalHistory();
     if (history !== null) {
         // Set the searchHistory array and update the display
-        searchHistory = history
-        searchHistory.forEach(() => updatePokemonHistoryTag())
+        searchHistory = history;
+        searchHistory.forEach(updateHistory);
     }
 
     // Install the service worker
