20b934a3a3ef5a2a1fda92200340847f2c692aa7
sgcarreno <secarro@hotmail.com>
2020-05-13 12:06:45 -0400

    /sw

--- tarea4/src/app.js
+++ tarea4/src/app.js
@@ -260,7 +260,7 @@ async function onLoadAsync() {
     // Install the service worker
     if ('serviceWorker' in navigator) {
         try {
-            let serviceWorker = await navigator.serviceWorker.register('../sw.js')
+            let serviceWorker = await navigator.serviceWorker.register('/sw.js')
             console.log(`Service worker registered ${serviceWorker}`)
         } catch (err) {
             console.error(`Failed to register service worker: ${err}`)
